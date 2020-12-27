ec1c750ca11d1aed0b2bc0e6949c6cda27b0faff
celestian <petr.celestian@gmail.com>
2020-12-27 12:22:03 +0100

    Obtain file list in .gtm archive
    .gtm file is typically .zip file containing a lot of .xml files. After
    extracting we should kow which files we have (because it differs for
    different .gtm files)
    
    Right now, we have the list of files in .gtm file in variable gtm_files.
    
    Next step is reasing the .xml files and saving usefull pieces
    of information to the database.

--- gtn-server/__main__.py
+++ gtn-server/__main__.py
@@ -1,7 +1,7 @@
 """GTN
 
 Usage:
-  gtn <input_gtm_file> <output_directory>
+  gtn <input_gtm_file>
   gtn (-h | --help)
   gtn --version
 
@@ -11,6 +11,8 @@ Options:
 """
 
 from docopt import docopt
+import os
+import tempfile
 import zipfile
 
 
@@ -18,9 +20,20 @@ def main():
 
     args = docopt(__doc__, version='0.0.1')
 
-    with zipfile.ZipFile(args['<input_gtm_file>'], 'r') as zip_ref:
-        zip_ref.extractall(args['<output_directory>'])
+    # Extract the .gtm file and get the file list
+    with tempfile.TemporaryDirectory() as tmp_dirname:
+        gtm_files = {} # file list
+        with zipfile.ZipFile(args['<input_gtm_file>'], 'r') as zip_ref:
+            zip_ref.extractall(tmp_dirname)
+            for root, dirs, files in os.walk(tmp_dirname):
+                name = 'gtm' if root == tmp_dirname else os.path.basename(os.path.normpath(root))
+                gtm_files[name] = []
+                for item in files:
+                    gtm_files[name].append(os.path.join(root, item))
 
+        # ... and here we can go throught the files and read them for saving usefull information to our database
+        # for future usage
+        print(gtm_files)
 
 
 if __name__ == '__main__':
